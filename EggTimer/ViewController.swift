0b4ad48fe3f645d9c851c25a92b19f868fad61a7
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2022-01-01 10:46:41 +0900

    ボタンタイトル修正

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -4,16 +4,52 @@
 //
 //  Created by Hiroaki-Hirabayashi on 2022/01/01.
 //
-
+import AVFoundation
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    let eggTimes = ["柔らかめ 3分": 3, "普通 5分": 5, "固め 7分": 7]
+    var timer = Timer()
+    var player: AVAudioPlayer!
+    var totalTime = 0
+    var secondsPassed = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func selectButton(_ sender: UIButton) {
+        // タイマーを止める
+        timer.invalidate()
+        // ボタンタイトル取得
+        let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target:self, selector: #selector(updateTimer), userInfo:nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+            print(Float(secondsPassed) / Float(totalTime))
+        } else {
+            timer.invalidate()
+            titleLabel.text = "出来ました!"
+            
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+        }
+    }
+    
 }
 
